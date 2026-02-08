08481912ebf4573ca4220380282f66ff294468a9
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2026-02-09 01:15:12 +0530

    bugfix: hydration-issue

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,10 +6,7 @@ import Navigation from "../Utils/Navigation/Navigation";
 export const Content: React.FC = () => {
   return (
     <div className="h-screen w-full">
-      <div
-        className="relative w-full min-h-full bg-white bg-[radial-gradient(circle,rgba(0,0,0,0.12)_1px,transparent_1.2px)]
-        bg-size-[22px_22px]"
-      >
+      <div className="relative w-full min-h-full bg-white bg-[radial-gradient(circle,rgba(0,0,0,0.12)_1px,transparent_1.2px)] bg-size-[22px_22px]">
         <div>
           <Navigation />
           <div className="mx-auto max-w-360 w-full">
